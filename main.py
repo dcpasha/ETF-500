de8ef2204e60b2d5f594b2e3b7cb4c7647ef8a97
dcpasha <66342395+dcpasha@users.noreply.github.com>
2020-10-08 12:02:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,49 @@
-import pandas as pd
-import numpy as np
+import pandas_datareader.data as web # To import financial data directly into python
 import matplotlib.pyplot as plt
-import seaborn as sns
 import scipy
 import openpyxl  # To save file to the excel
 
-# TODO
-# 1) Make Sure that data is the same. YES, it is the same.
-# 2) Make a list of ETFs and import data in a good format
-# 3) Compare them during different time interval
+start = "2015-01-01"
+end = "2020-10-07"
 
-# Package to import financial data directly into python
-import pandas_datareader.data as web
-
-start = "2020-01-01"
-end = "2020-10-04"
 
 SP500_ETF = ["VOO", "VV", "MGC", "IVV", "SCHX", "SPY", "SPLG"]
 
+# Save the data in etf
 etfs = web.DataReader(SP500_ETF, 'yahoo', start, end)
-# df.to_excel("/Users/pavelpotapov/Data/SP500/My_findings.xlsx") # To save file to the excel
 
-# print(etfs.info())
-# print(etfs["Adj Close"].head())
+# The Adjusted Closing Price that shows the ETF's value after posting a dividend will be used
 etfs = etfs["Adj Close"]
-# print(etfs.head())
-transformed_etf = etfs.pct_change()[1:]
-# print(transformed_etf.head())
+plt.figure()
+etfs.plot(ylabel="Daily Price")
+# It is hard to visually compare different graphs. Your eyes can see trends that do not exist.
 
-# voo_data = pd.read_csv("/Users/pavelpotapov/Data/SP500/VOO.csv")  # it is a dataframe
-# ivv_data = pd.read_csv("/Users/pavelpotapov/Data/SP500/IVV.csv")  # it is a dataframe
-#
-#
-# # Creating index of a dataframe
-# # print(voo_data.head())  # "Date" was just a column
-# voo_data = voo_data.set_index("Date")
-# ivv_data = ivv_data.set_index("Date")
-#
-#
-# # # Selecting certain rows of a dataframe based on index
-# # voo_data = voo_data[voo_data.index > "2018-01-01"]
-#
-# # print("Creating a Series")
-# voo_series = pd.Series(voo_data["Close"], name="VOO")
-# ivv_series = pd.Series(ivv_data["Close"], name="VOO")
-#
-#
-# # # Selecting certain rows of a Series based on index
-# voo_series = voo_series[voo_series.index > '2020-01-01']
-# ivv_series = ivv_series[ivv_series.index > '2020-01-01']
-#
-#
-# # Take Percentage change
-# voo_pct = voo_series.pct_change()[1:]
-# ivv_pct = ivv_series.pct_change()[1:]
-#
+# It is better to look at a percentage change in a stock/ETF price than to look at how much it changed in price in
+# dollars per share. That is because it is easier to compare them to alternatives.
+etfs_pct = etfs.pct_change()[1:]
+print(etfs_pct.head())
+plt.figure()
+etfs_pct.plot(ylabel="Daily % Change")
+plt.show()
 
 # # ANOVA
 # # Check assumptions:
-# # 1) Residuals (experimental error) are normally distributed (Shapiro-Wilks Test)
-# # 2) Homogeneity of variances (variances are equal between treatment groups) (Levene or Bartlett Test)
-# # 3) Data are independent.
-#
-# # Shapiro-Wilks Test test the null hypo that the data was drawn from a normal distribution.
-# shapiro_test = scipy.stats.shapiro(voo_pct)
-# # print(shapiro_test.pvalue)  # 1.501246002511003e-11
-# # Since p-values is less than 0.05, we reject the null hypothesis that the data was drawn from normal distribution.
-# # Thus, we violates a normality assumption of the Anova distribution, and we won't use it.
-#
-# # Since the data does not have a normal distribution,
-# # use Kruskal–Wallis one-way analysis of variance
-print(scipy.stats.kruskal(transformed_etf.iloc[:,1],transformed_etf.iloc[:,2],transformed_etf.iloc[:,3],transformed_etf.iloc[:,4]))
-# print(transformed_etf.iloc[:,1].head())
+# 1) Residuals (experimental error) are normally distributed (Shapiro-Wilks Test)
+# 2) Homogeneity of variances (variances are equal between treatment groups) (Levene or Bartlett Test)
+# 3) Data are independent.
+# Shapiro-Wilks Test test the null hypo that the data was drawn from a normal distribution.
 
+shapiro_test_voo = scipy.stats.shapiro(etfs_pct['VOO'])
+shapiro_test_ivv = scipy.stats.shapiro(etfs_pct['IVV'])
+
+print(shapiro_test_voo.pvalue) # 2.9823333551374853e-38
+print(shapiro_test_ivv.pvalue) # 4.044263956073654e-38
+
+# Since p-values is less than 0.05, we reject the null hypothesis that the data was drawn from normal distribution.
+# Thus, we violates a normality assumption of the Anova distribution, and we won't use ANOVA.
+# Since the data does not have a normal distribution, use Kruskal–Wallis one-way analysis of variance
+kruskalWallis = (scipy.stats.kruskal(etfs_pct["VOO"],etfs_pct["VV"],etfs_pct["MGC"],etfs_pct["IVV"],etfs_pct["SCHX"],
+                                      etfs_pct["SPY"],etfs_pct["SPLG"]))
+print(kruskalWallis.pvalue)
+# The p-value is 0.9999952, so we fail to reject the null hypothesis 
+# that assumes that the samples (groups) are from identical populations.
